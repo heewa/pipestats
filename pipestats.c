ec65797f90718afd51822d5d9325d09888aaf621
Heewa Barfchin <heewa.b@gmail.com>
2014-07-21 01:28:54 +0200

    Decouple reporting & collecting stats, and improved final report.
    Use a timer with signals to report outside the main thread of moving
    data & gathering stats. That allows using blocking reads, which is
    nicer to the CPU, instead of constantly checking the buffer in a super
    tight loop. Also is more consistent, probably, if signals can be handled
    while data is being moved.

--- pipestats.c
+++ pipestats.c
@@ -1,57 +1,85 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
+#include <string.h>
 #include <sys/time.h>
+#include <time.h>
+#include <signal.h>
 
 #define BUF_SIZE (1024)
 #define REPORT_FREQ (2.0)
 
 double elapsed_sec(struct timeval* end, struct timeval* start);
-void print_report(double elapsed, unsigned int bytes_since, unsigned long int total_bytes);
+void print_report();
+void print_final_report();
+void interval(int signal);
+void cleanup(int signal);
+
+
+typedef struct Stats {
+    unsigned long int total_bytes;
+    unsigned int bytes_since;
+    struct timeval last_report;
+    struct timeval start;
+} Stats;
+Stats stats;
+
 
 int main(int argc, char** argv) {
     char buff[BUF_SIZE];
-    unsigned long int total_bytes = 0;
-    unsigned int bytes_since = 0;
-    struct timeval last_report;
-    double elapsed;
+    struct itimerval interval_timer;
+    struct sigaction interval_action;
+    struct sigaction cleanup_action;
 
-    // Go into non-blocking mode, so even if there's less than buffer size of
-    // data, we clean that out and report stats on it.
-    fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+    // Init stats.
+    memset(&stats, 0, sizeof(Stats));
+    gettimeofday(&stats.start, NULL);
+    stats.last_report = stats.start;
 
-    gettimeofday(&last_report, NULL);
+    // Init timer. Use a timer instead of reporting in the main loop so we
+    // can use blocking reads (while reporting consistently), instead of
+    // a wasteful tight loop.
+    interval_timer.it_interval.tv_sec = (int) REPORT_FREQ;
+    interval_timer.it_interval.tv_usec =
+        (REPORT_FREQ - ((int) REPORT_FREQ)) * (1000 * 1000);
+    interval_timer.it_value = interval_timer.it_interval;
+
+    memset(&interval_action, 0, sizeof(struct sigaction));
+    interval_action.sa_handler = &interval;
+    if (sigaction(SIGALRM, &interval_action, NULL) != 0) {
+        fprintf(stderr, "Failed to set up timer handler.\n");
+        return -1;
+    }
+    if (setitimer(ITIMER_REAL, &interval_timer, NULL) != 0) {
+        fprintf(stderr, "Failed to start timer.\n");
+        return -1;
+    }
+
+    // Set up handler for exiting, to print a final report, even if aborted.
+    memset(&cleanup_action, 0, sizeof(struct sigaction));
+    cleanup_action.sa_handler = &cleanup;
+    if (sigaction(SIGINT, &cleanup_action, NULL) != 0) {
+        fprintf(stderr, "Failed to set cleanup handler.\n");
+        return -1;
+    }
 
     // Read until there's nothing left.
     while (!feof(stdin)) {
         int bytes_read = 0;
-        struct timeval now;
 
         // Read and write out as close to each other as possible.
         bytes_read = fread(buff, 1, BUF_SIZE, stdin);
         if (bytes_read > 0) {
-            total_bytes += bytes_read;
-            bytes_since += bytes_read;
+            // But first update counters, cuz we might report white this is
+            // going out to the buffer? Is that sound logic? woof!
+            stats.total_bytes += bytes_read;
+            stats.bytes_since += bytes_read;
             fwrite(buff, 1, bytes_read, stdout);
         }
-
-        gettimeofday(&now, NULL);
-        elapsed = elapsed_sec(&now, &last_report);
-        if (elapsed >= REPORT_FREQ) {
-            print_report(elapsed, bytes_since, total_bytes);
-            bytes_since = 0;
-            last_report = now;
-            elapsed = 0;
-        }
     }
 
-    if (elapsed > 1.0) {
-        print_report(elapsed, bytes_since, total_bytes);
-    } else {
-        // Just print a final report.
-        fprintf(stderr, "%lu bytes total\n", total_bytes);
-    }
+    print_final_report();
 
     return 0;
 }
@@ -64,12 +92,46 @@ double elapsed_sec(struct timeval* end, struct timeval* start) {
 }
 
 
-void print_report(double elapsed, unsigned int bytes_since, unsigned long int total_bytes) {
+void print_report() {
+    struct timeval now;
+    double elapsed;
+
+    gettimeofday(&now, NULL);
+    elapsed = elapsed_sec(&now, &stats.last_report);
+
     fprintf(stderr,
             "%3.2f bytes per sec, "
             "%lu bytes total, "
             "%d since last report\n",
-            bytes_since / elapsed,
-            total_bytes,
-            bytes_since);
+            stats.bytes_since / elapsed,
+            stats.total_bytes,
+            stats.bytes_since);
+
+    stats.bytes_since = 0;
+    stats.last_report = now;
+}
+
+
+void print_final_report() {
+    struct timeval now;
+    double elapsed;
+
+    gettimeofday(&now, NULL);
+    elapsed = elapsed_sec(&now, &stats.start);
+
+    fprintf(stderr, "\n%lu bytes total over %.2f sec, avg %.2f bytes per sec\n",
+            stats.total_bytes,
+            elapsed,
+            stats.total_bytes / elapsed);
+}
+
+
+void interval(int signal) {
+    print_report();
+}
+
+
+void cleanup(int signal) {
+    print_final_report();
+    exit(0);
 }
