906522455638a4bee4eb11f2a5ebff76ab516c0d
Heewa Barfchin <heewa.b@gmail.com>
2014-07-21 14:40:28 +0200

    Units other than bytes, plus human.

--- pipestats.c
+++ pipestats.c
@@ -10,18 +10,6 @@
 
 #define BUF_SIZE (1024)
 
-double elapsed_sec(struct timeval* end, struct timeval* start);
-
-int read_options();
-int setup();
-
-void print_report();
-void print_final_report();
-
-void interval(int signal);
-void cleanup(int signal);
-
-
 typedef struct Stats {
     unsigned long int total_bytes;
     unsigned int bytes_since;
@@ -31,10 +19,38 @@ typedef struct Stats {
 Stats stats;
 
 
+typedef enum Unit {
+    Human = 0,
+    Bytes = 1,
+    Kilobytes = 1024,
+    Megabytes = 1024 * 1024,
+    Gigabytes = 1024 * 1024 * 1024,
+} Unit;
+
+
 typedef struct Options {
     double freq;
+    Unit unit;
 } Options;
 Options options;
+
+
+double elapsed_sec(struct timeval* end, struct timeval* start);
+double adjust_unit(double bytes, Unit target_unit);
+const char* unit_name(double bytes, Unit target_unit);
+Unit find_unit(double bytes);
+
+
+int read_options();
+int setup();
+
+
+void print_report();
+void print_final_report();
+
+
+void interval(int signal);
+void cleanup(int signal);
 
 
 int main(int argc, char** argv) {
@@ -83,17 +99,19 @@ int read_options(int argc, char** argv) {
     int opt = 0;
     static struct option long_options[] = {
         {"help", no_argument, NULL, 'h'},
+        {"human", no_argument, NULL, 'H'},
         {"freq", required_argument, NULL, 'f'},
         {0, 0, 0, 0}
     };
 
     // Init with defaults.
     options.freq = 2.0;
+    options.unit = Human;
 
     while (opt != -1) {
         int option_index = 0;
 
-        opt = getopt_long(argc, argv, "hf:", long_options, &option_index);
+        opt = getopt_long(argc, argv, "hHBKMGf:", long_options, &option_index);
         switch (opt) {
         case -1:
             break;
@@ -107,6 +125,26 @@ int read_options(int argc, char** argv) {
                    "and reports stats about data transfered to stderr.\n",
                    argv[0]);
             return -1;
+            break;
+
+        case 'H':
+            options.unit = Human;
+            break;
+
+        case 'B':
+            options.unit = Bytes;
+            break;
+
+        case 'K':
+            options.unit = Kilobytes;
+            break;
+
+        case 'M':
+            options.unit = Megabytes;
+            break;
+
+        case 'G':
+            options.unit = Gigabytes;
             break;
 
         case 'f':
@@ -186,20 +224,76 @@ double elapsed_sec(struct timeval* end, struct timeval* start) {
 }
 
 
+double adjust_unit(double bytes, Unit target_unit) {
+    if (target_unit != Human) {
+        return bytes / (double) target_unit;
+    }
+    return bytes / (double) find_unit(bytes);
+}
+
+
+Unit find_unit(double bytes) {
+    if (bytes >= Gigabytes) {
+        return Gigabytes;
+    } else if (bytes >= Megabytes) {
+        return Megabytes;
+    } else if (bytes >= Kilobytes) {
+        return Kilobytes;
+    }
+    return Bytes;
+}
+
+
+const char* unit_name(double bytes, Unit target_unit) {
+    if (target_unit == Human) {
+        target_unit = find_unit(bytes);
+    }
+
+    switch (target_unit) {
+    case Gigabytes:
+        return "GB";
+        break;
+
+    case Megabytes:
+        return "MB";
+        break;
+
+    case Kilobytes:
+        return "KB";
+        break;
+
+    case Bytes:
+        return "B";
+        break;
+
+    default:
+        break;
+    }
+
+    return "??";
+}
+
+
 void print_report() {
     struct timeval now;
     double elapsed;
+
+    double data_amount_since = adjust_unit(stats.bytes_since, options.unit);
+    const char* data_amount_since_unit = unit_name(stats.bytes_since, options.unit);
+
+    double data_amount_total = adjust_unit(stats.total_bytes, options.unit);
+    const char* data_amount_total_unit = unit_name(stats.total_bytes, options.unit);
 
     gettimeofday(&now, NULL);
     elapsed = elapsed_sec(&now, &stats.last_report);
 
     fprintf(stderr,
-            "%3.2f bytes per sec, "
-            "%lu bytes total, "
-            "%d since last report\n",
-            stats.bytes_since / elapsed,
-            stats.total_bytes,
-            stats.bytes_since);
+            "%3.2f %s/s, "
+            "%3.2f %s total, "
+            "%3.2f %s since last report\n",
+            data_amount_since / elapsed, data_amount_since_unit,
+            data_amount_total, data_amount_total_unit,
+            data_amount_since, data_amount_since_unit);
 
     stats.bytes_since = 0;
     stats.last_report = now;
@@ -210,13 +304,17 @@ void print_final_report() {
     struct timeval now;
     double elapsed;
 
+    double data_amount = adjust_unit(stats.total_bytes, options.unit);
+    const char* data_amount_unit = unit_name(stats.total_bytes, options.unit);
+
     gettimeofday(&now, NULL);
     elapsed = elapsed_sec(&now, &stats.start);
 
-    fprintf(stderr, "%lu bytes total over %.2f sec, avg %.2f bytes per sec\n",
+    fprintf(stderr, "%3.2f %s (%lu bytes) total over %.2f sec, avg %.2f %s/s\n",
+            data_amount, data_amount_unit,
             stats.total_bytes,
             elapsed,
-            stats.total_bytes / elapsed);
+            data_amount / elapsed, data_amount_unit);
 }
 
 
